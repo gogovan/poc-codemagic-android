a8a72391ae622afa38786e6aef73cc26b30f4640
GOGOVAN-Ivo Wu <ivo.wu@gogotech.hk>
2022-02-23 16:33:41 +0800

    init project

---
+++ app/src/main/java/hk/gogovan/codemagicpoc/MainActivity.kt
@@ -0,0 +1,11 @@
+package hk.gogovan.codemagicpoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
